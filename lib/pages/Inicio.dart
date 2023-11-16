b0531711d972622f9872ba8d81b9a39d53bfd745
JEHRErnesto <ernestoramos1721@gmail.com>
2023-11-15 22:21:13 -0600

    agregando el método de pintar la ruta

--- lib/pages/Inicio.dart
+++ lib/pages/Inicio.dart
@@ -17,6 +17,95 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
 
   late GoogleMapController _googleMapController;
+  Set<Marker> _markers = {};
+  Set<google_maps_flutter.Polyline> _polylines = {};
+  PolylinePoints polylinePoints = PolylinePoints();
+  Position? _currentPosition;
+
+  final directionsApiClient =
+      GoogleMapsDirections(apiKey: 'AIzaSyD57kSeEEU11CaUK30VHYl-8i9M58ePwP8');
+
+
+  void _calculateAndDisplayRoute(LatLng destination) async {
+    if (_currentPosition == null) {
+      return;
+    }
+
+    PointLatLng origin =
+        PointLatLng(_currentPosition!.latitude, _currentPosition!.longitude);
+    PointLatLng destinationPoint =
+        PointLatLng(destination.latitude, destination.longitude);
+
+    DirectionsResponse response = await directionsApiClient.directions(
+      Location(lat: origin.latitude, lng: origin.longitude),
+      Location(lat: destinationPoint.latitude, lng: destinationPoint.longitude),
+      travelMode: google_directions.TravelMode.driving,
+    );
+
+    if (response.status == 'OK') {
+      List<LatLng> route = [];
+
+      for (var step in response.routes[0].legs[0].steps) {
+        final startLocation = step.startLocation;
+        final endLocation = step.endLocation;
+
+        route.add(LatLng(startLocation.lat, startLocation.lng));
+        route.add(LatLng(endLocation.lat, endLocation.lng));
+      }
+
+      // Borra las polilíneas existentes
+      _polylines.clear();
+
+      // Añade la nueva polilínea
+      setState(() {
+        _polylines.add(google_maps_flutter.Polyline(
+          polylineId: const PolylineId('polyline'),
+          points: route,
+          width: 5,
+          color: Colors.red,
+        ));
+      });
+
+      // Ajusta la cámara para que muestre toda la ruta
+      LatLngBounds bounds = LatLngBounds(
+        southwest: LatLng(min(origin.latitude, destinationPoint.latitude),
+            min(origin.longitude, destinationPoint.longitude)),
+        northeast: LatLng(max(origin.latitude, destinationPoint.latitude),
+            max(origin.longitude, destinationPoint.longitude)),
+      );
+      _googleMapController
+          .animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
+    } else {
+      print('Error al calcular la ruta: ${response.errorMessage}');
+    }
+  }
+
+// Método para actualizar la ruta en función de la nueva posición del usuario
+  void _updateRoute(LatLng currentPosition) {
+    if (_polylines.isNotEmpty) {
+      // Convierte el conjunto a una lista
+      List<google_maps_flutter.Polyline> polylinesList = _polylines.toList();
+
+      // Obtén la última polilínea
+      List<LatLng> existingRoute = polylinesList[0].points;
+
+      // Añade la nueva posición del usuario a la ruta existente
+      existingRoute
+          .add(LatLng(currentPosition.latitude, currentPosition.longitude));
+
+      // Actualiza la polilínea
+      setState(() {
+        _polylines.clear();
+        _polylines.add(google_maps_flutter.Polyline(
+          polylineId: const PolylineId('polyline'),
+          points: existingRoute,
+          width: 5,
+          color: Colors.red,
+        ));
+      });
+    }
+  }
+
 
   Future<Position> determinePosition() async {
     LocationPermission permission;
@@ -29,6 +118,9 @@ class _MapPageState extends State<MapPage> {
     }
     return await Geolocator.getCurrentPosition();
   }
+
+
+
 
   @override
   Widget build(BuildContext context) {
